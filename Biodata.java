7f8b7c03cd22b1f1ccbb5cd81d9389ba675616fc
rachmadrf <rachmad17845@gmail.com>
2024-09-05 14:54:31 +0700

    ganti println TTL menjadi Hobi

--- Biodata.java
+++ Biodata.java
@@ -6,7 +6,7 @@ System.out.println("-- Biodata Mahasiswa --");
 System.out.println("Nama\t:");
 System.out.println("NIM\t:");
 System.out.println("Kelas\t:");
-System.out.println("TTL\t:"); 
+System.out.println("Hobi\t:"); 
 
     }
 }
