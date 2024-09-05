dee1b60bb136d594b79ed2bdb07b89fd38e01e15
Yahya1805 <belajarkuliah902@gmail.com>
2024-09-05 16:53:11 +0700

    Delete biodata3.java

--- biodata3.java
+++
@@ -1,7 +0,0 @@
-public class biodata1 {
-
-    public static void main(String[] args) {
-        System.out.println("saya M.YAHYA.IRVANSYAH");
-        System.out.println("dan saya bersalah dari kelas 1F");
-    }
-}
