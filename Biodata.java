c9b00ffa1c79f7dc7a87941ce475f5e58300d5e9
rachmadrf <rachmad17845@gmail.com>
2024-09-05 14:47:20 +0700

    simpan kode java ke Github

---
+++ Biodata.java
@@ -0,0 +1,12 @@
+public class Biodata {
+
+    public static void main(String[] args) {
+        
+System.out.println("-- Biodata Mahasiswa --");
+System.out.println("Nama\t:");
+System.out.println("NIM\t:");
+System.out.println("Kelas\t:");
+System.out.println("TTL\t:"); 
+
+    }
+}
