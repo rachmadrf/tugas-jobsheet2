1b7b55d23932aad283fecae53c954ded3b4cb2e0
Yahya1805 <belajarkuliah902@gmail.com>
2024-09-05 16:49:15 +0700

    Add files via upload

---
+++ biodata3.java
@@ -0,0 +1,7 @@
+public class biodata1 {
+
+    public static void main(String[] args) {
+        System.out.println("saya M.YAHYA.IRVANSYAH");
+        System.out.println("dan saya bersalah dari kelas 1F");
+    }
+}
