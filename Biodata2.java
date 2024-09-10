fedcf7a235ade3b6fb23a91674932ef78a01127f
RifkyP30 <rifkypraditya1025@gmail.com>
2024-09-10 13:57:46 +0700

    Add files via upload

---
+++ Biodata2.java
@@ -0,0 +1,9 @@
+public class Biodata2 {
+  public static void main(String[] args) {
+    System.out.println("---Biodata Mahasiswa 2---");
+    System.out.println("Nama\t:  Muhammad Rifky Praditya");
+    System.out.println("NIM\t:  244107020096");
+    System.out.println("Kelas\t:  1F");
+    System.out.println("Hobi\t:  Nonton film/series");
+  }
+}
