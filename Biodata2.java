312f72af764342c863dde6dd448a6737fddffd02
Rachmad <108579144+rachmadrf@users.noreply.github.com>
2024-09-10 19:59:41 +0700

    Delete Biodata2.java

--- Biodata2.java
+++
@@ -1,9 +0,0 @@
-public class Biodata2 {
-  public static void main(String[] args) {
-    System.out.println("---Biodata Mahasiswa 2---");
-    System.out.println("Nama\t:  Muhammad Rifky Praditya");
-    System.out.println("NIM\t:  244107020096");
-    System.out.println("Kelas\t:  TI-1F");
-    System.out.println("Hobi\t:  Nonton film/series");
-  }
-}
