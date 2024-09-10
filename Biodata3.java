54efe98df1d9f3698e870d411485e7f2bd83dc65
Rachmad <108579144+rachmadrf@users.noreply.github.com>
2024-09-10 19:06:55 +0700

    Update Biodata3.java

--- Biodata3.java
+++ Biodata3.java
@@ -2,7 +2,7 @@ public class Biodata {
 
     public static void main(String[] args) {
         
-        System.out.println("-- Biodata Mahasiswa 1 --");
+        System.out.println("-- Biodata Mahasiswa 3 --");
         System.out.println("Nama\t: M.Yahya.Irvansyah");
         System.out.println("NIM\t: 244107020032");
         System.out.println("Kelas\t: TI-1F");
