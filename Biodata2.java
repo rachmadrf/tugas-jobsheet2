57dab488c1fa76ad8d801b0e24eb179903b3706e
Rachmad <108579144+rachmadrf@users.noreply.github.com>
2024-09-10 19:08:06 +0700

    Update Biodata2.java

--- Biodata2.java
+++ Biodata2.java
@@ -3,7 +3,7 @@ public class Biodata2 {
     System.out.println("---Biodata Mahasiswa 2---");
     System.out.println("Nama\t:  Muhammad Rifky Praditya");
     System.out.println("NIM\t:  244107020096");
-    System.out.println("Kelas\t:  1F");
+    System.out.println("Kelas\t:  TI-1F");
     System.out.println("Hobi\t:  Nonton film/series");
   }
 }
