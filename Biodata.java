ceab208ca842c2926dcda71d866a79c65bc21f9e
Rachmad <108579144+rachmadrf@users.noreply.github.com>
2024-09-11 13:50:55 +0700

    Update Biodata.java

--- Biodata.java
+++ Biodata.java
@@ -8,34 +8,18 @@ public class Biodata {
         System.out.println("Kelas\t: TI-1F");
         System.out.println("Hobi\t: Musik"); 
 
-    }
-}
+        System.out.println("---Biodata Mahasiswa 2---");
+        System.out.println("Nama\t:  Muhammad Rifky Praditya");
+        System.out.println("NIM\t:  244107020096");
+        System.out.println("Kelas\t:  TI-1F");
+        System.out.println("Hobi\t:  Nonton film/series");
 
-public class Biodata2 {
-  public static void main(String[] args) {
-    System.out.println("---Biodata Mahasiswa 2---");
-    System.out.println("Nama\t:  Muhammad Rifky Praditya");
-    System.out.println("NIM\t:  244107020096");
-    System.out.println("Kelas\t:  TI-1F");
-    System.out.println("Hobi\t:  Nonton film/series");
-  }
-}
-
-public class Biodata {
-
-    public static void main(String[] args) {
-        
         System.out.println("-- Biodata Mahasiswa 3 --");
         System.out.println("Nama\t: M.Yahya.Irvansyah");
         System.out.println("NIM\t: 244107020032");
         System.out.println("Kelas\t: TI-1F");
         System.out.println("Hobi\t: bulutangkis"); 
 
-    }
-}
-
-public class Biodata4 {
-    public static void main(String[] args) {
         System.out.println("-- Biodata Mahasiswa 4 --");
         System.out.println("Nama\t: Gilang Bayu Irwana");
         System.out.println("NIM\t: 244107020194");
